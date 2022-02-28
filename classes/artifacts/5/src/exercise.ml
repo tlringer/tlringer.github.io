55535408cfe57ef982e1b11de5a8443cbb531590
Talia Ringer <talia@dependenttyp.es>
2022-02-27 22:06:59 -0600

    weee

--- classes/artifacts/5/src/exercise.ml
+++ classes/artifacts/5/src/exercise.ml
@@ -7,6 +7,8 @@ open Termutils
  * (no pattern matching, no fixpoints, no lets, and so on).
  *
  * I have done this one for you, to help you figure out how to implement sub.
+ * I tried to use the state monad only when I felt like it added clarity,
+ * but if you find it confusing, please ask for help.
  *)
 let rec count env src trm sigma =
   let sigma, is_eq = Termutils.equal env src trm sigma in
@@ -14,19 +16,22 @@ let rec count env src trm sigma =
     sigma, 1
   else
     match kind sigma trm with
-    | Constr.Lambda (n, t, b) | Constr.Prod (n, t, b) ->
+    | Constr.Lambda (n, t, b) -> (* fun (n : t) => b *)
        let sigma, count_t = count env src t sigma in
        let sigma, count_b = count (push_local (n, t) env) src b sigma in
        sigma, count_t + count_b
-    | Constr.App (f, args) ->
-       let sigma_f, occs_f = count env src f sigma in
-       Termutils.fold_left_state_array
-         (fun occs arg sigma ->
-           let sigma, occs_arg = count env src arg sigma in
-           sigma, occs_arg + occs)
-         occs_f
-         args
-         sigma_f
+    | Constr.Prod (n, t, b) -> (* forall (n : t), b *)
+       let sigma, count_t = count env src t sigma in
+       let sigma, count_b = count (push_local (n, t) env) src b sigma in
+       sigma, count_t + count_b
+    | Constr.App (f, args) -> (* f args *)
+       let sigma, count_f = count env src f sigma in
+       let sigma, count_args =
+         Termutils.map_state_array
+           (count env src)
+           args
+           sigma
+       in sigma, Array.fold_left (fun b a -> b + a) count_f count_args
     | _ ->
        sigma, 0
 
@@ -34,24 +39,36 @@ let rec count env src trm sigma =
  * Substitute all occurrences of terms equal to src in trm with dst.
  * Make some simplifying assumptions about the format of trm
  * (no pattern matching, no fixpoints, not lets, and so on).
+ *
+ * HINT 1: You will want to use the mkLambda, mkProd, and mkApp functions 
+ * defined in EConstr: https://github.com/coq/coq/blob/v8.14/engine/eConstr.mli
+ *
+ * HINT 2: If you are totally stuck, try copying and pasting the body of
+ * each case of count, then adapting it to return the substituted term
+ * instead of a number. The function will have almost exactly the same
+ * structure.
  *)
 let rec sub env (src, dst) trm sigma =
   let sigma, is_eq = Termutils.equal env src trm sigma in
   if is_eq then
     sigma, dst
   else
-    match EConstr.kind sigma trm with
-    | Constr.Lambda (n, t, b) | Constr.Prod (n, t, b) ->
+    match kind sigma trm with
+    | Constr.Lambda (n, t, b) -> (* fun (n : t) => b *)
        let sigma, sub_t = sub env (src, dst) t sigma in
        let sigma, sub_b = sub (push_local (n, t) env) (src, dst) b sigma in
-       sigma, EConstr.mkLambda (n, sub_t, sub_b)
-    | Constr.App (f, args) ->
-       let sigma_f, subbed_f = sub env (src, dst) f sigma in
-       let sigma_args, subbed_args =
+       sigma, mkLambda (n, sub_t, sub_b)
+    | Constr.Prod (n, t, b) -> (* prod (n : t) => b *)
+       let sigma, sub_t = sub env (src, dst) t sigma in
+       let sigma, sub_b = sub (push_local (n, t) env) (src, dst) b sigma in
+       sigma, mkProd (n, sub_t, sub_b)
+    | Constr.App (f, args) -> (* f args *)
+       let sigma, sub_f = sub env (src, dst) f sigma in
+       let sigma, sub_args =
          Termutils.map_state_array
            (sub env (src, dst))
            args
-           sigma_f
-       in sigma_args, EConstr.mkApp (subbed_f, subbed_args)
+           sigma
+       in sigma, mkApp (sub_f, sub_args)
     | _ ->
        sigma, trm
--- classes/artifacts/5/src/g_tuto1.ml
+++ classes/artifacts/5/src/g_tuto1.ml
@@ -10,6 +10,7 @@ let _ = Mltop.add_known_module __coq_plugin_name
  *)
 open Pp
 open Stdarg
+open Termutils
 open Exercise
 
 
@@ -22,67 +23,30 @@ let () = Vernacextend.vernac_extend ~command:"MyDefine" ~classifier:(fun _ -> Ve
                                                                     Vernacextend.TyNil)))), 
          (let coqpp_body i e
          () = Vernacextend.VtDefault (fun () -> 
-# 38 "src/g_tuto1.mlg"
+# 39 "src/g_tuto1.mlg"
     
-     let (sigma, env) = Termutils.global_env () in
-     let (sigma, t) = Termutils.internalize env e sigma in
-     Termutils.define i t sigma
+     let sigma, env = global_env () in
+     let sigma, t = internalize env e sigma in
+     define i t sigma
    
               ) in fun i
          e ?loc ~atts () -> coqpp_body i e
          (Attributes.unsupported_attributes atts)), None))]
 
-let () = Vernacextend.vernac_extend ~command:"Arity" ~classifier:(fun _ -> Vernacextend.classify_as_sideeff) ?entry:None 
-         [(Vernacextend.TyML (false, Vernacextend.TyTerminal ("Arity", 
-                                     Vernacextend.TyNonTerminal (Extend.TUentry (Genarg.get_arg_tag wit_constr), 
-                                     Vernacextend.TyNil)), (let coqpp_body e
-                                                           () = Vernacextend.VtDefault (fun () -> 
-                                                                
-# 56 "src/g_tuto1.mlg"
-    
-     let (sigma, env) = Termutils.global_env () in
-     let (sigma, t) = Termutils.internalize env e sigma in
-     let (sigma, arity) = arity t sigma in
-     Feedback.msg_notice (strbrk (string_of_int arity))
-   
-                                                                ) in fun e
-                                                           ?loc ~atts ()
-                                                           -> coqpp_body e
-                                                           (Attributes.unsupported_attributes atts)), None))]
-
-let () = Vernacextend.vernac_extend ~command:"Nargs" ~classifier:(fun _ -> Vernacextend.classify_as_sideeff) ?entry:None 
-         [(Vernacextend.TyML (false, Vernacextend.TyTerminal ("Nargs", 
-                                     Vernacextend.TyNonTerminal (Extend.TUentry (Genarg.get_arg_tag wit_constr), 
-                                     Vernacextend.TyNil)), (let coqpp_body e
-                                                           () = Vernacextend.VtDefault (fun () -> 
-                                                                
-# 71 "src/g_tuto1.mlg"
-    
-     let (sigma, env) = Termutils.global_env () in
-     let (sigma, t) = Termutils.internalize env e sigma in
-     let (sigma, nargs) = nargs t sigma in
-     Feedback.msg_notice (strbrk (string_of_int nargs))
-   
-                                                                ) in fun e
-                                                           ?loc ~atts ()
-                                                           -> coqpp_body e
-                                                           (Attributes.unsupported_attributes atts)), None))]
-
 let () = Vernacextend.vernac_extend ~command:"Count" ~classifier:(fun _ -> Vernacextend.classify_as_sideeff) ?entry:None 
          [(Vernacextend.TyML (false, Vernacextend.TyTerminal ("Count", 
                                      Vernacextend.TyNonTerminal (Extend.TUentry (Genarg.get_arg_tag wit_constr), 
-                                     Vernacextend.TyTerminal ("in", Vernacextend.TyTerminal ("body", 
-                                                                    Vernacextend.TyNonTerminal (
+                                     Vernacextend.TyTerminal ("in", Vernacextend.TyNonTerminal (
                                                                     Extend.TUentry (Genarg.get_arg_tag wit_constr), 
-                                                                    Vernacextend.TyNil))))), 
+                                                                    Vernacextend.TyNil)))), 
          (let coqpp_body e1 e2
          () = Vernacextend.VtDefault (fun () -> 
-# 84 "src/g_tuto1.mlg"
+# 53 "src/g_tuto1.mlg"
     
-     let (sigma, env) = Termutils.global_env () in
-     let (sigma, t1) = Termutils.internalize env e1 sigma in
-     let (sigma, t2) = Termutils.internalize env e2 sigma in
-     let (sigma, count) = count_in_body env t1 t2 sigma in
+     let sigma, env = global_env () in
+     let sigma, t1 = internalize env e1 sigma in
+     let sigma, t2 = internalize env e2 sigma in
+     let sigma, count = count env t1 t2 sigma in
      Feedback.msg_notice (strbrk (string_of_int count))
    
               ) in fun e1
@@ -102,23 +66,23 @@ let () = Vernacextend.vernac_extend ~command:"Sub" ~classifier:(fun _ -> Vernace
                                                                     Vernacextend.TyNonTerminal (
                                                                     Extend.TUentry (Genarg.get_arg_tag wit_ident), 
                                                                     Vernacextend.TyNil)))))))), 
-         (let coqpp_body e1s e2s e3 i
+         (let coqpp_body src_es dst_es e i
          () = Vernacextend.VtDefault (fun () -> 
-# 100 "src/g_tuto1.mlg"
+# 70 "src/g_tuto1.mlg"
     
-     let (sigma, env) = Termutils.global_env () in
-     let (sigma, t1s) = Termutils.map_state (Termutils.internalize env) e1s sigma in
-     let (sigma, t2s) = Termutils.map_state (Termutils.internalize env) e2s sigma in
-     let (sigma, t3) = Termutils.internalize env e3 sigma in
-     let (sigma, subbed) =
-       Termutils.fold_left_state
-         (fun subbed (t1, t2) -> sub_in env (t1, t2) subbed)
-         t3
-         (List.combine t1s t2s)
+     let sigma, env = global_env () in
+     let sigma, srcs = map_state (internalize env) src_es sigma in
+     let sigma, dsts = map_state (internalize env) dst_es sigma in
+     let sigma, trm = internalize env e sigma in
+     let sigma, subbed =
+       fold_left_state
+         (fun subbed (src, dst) -> sub env (src, dst) subbed)
+         trm
+         (List.combine srcs dsts)
          sigma
      in Termutils.define i subbed sigma
    
-              ) in fun e1s
-         e2s e3 i ?loc ~atts () -> coqpp_body e1s e2s e3 i
+              ) in fun src_es
+         dst_es e i ?loc ~atts () -> coqpp_body src_es dst_es e i
          (Attributes.unsupported_attributes atts)), None))]
 
